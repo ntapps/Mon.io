0b892961be8b799354608b2df1883f7ac79d1a1d
Andrew Lehman <lehmanad1@live.com>
2018-05-19 21:21:06 -0400

    Re-adding angular after weird .gitignore concerns

---
+++ MonIOWebApp/ClientApp/src/app/savings/savings.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SavingsRoutingModule } from './savings-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SavingsRoutingModule
+  ],
+  declarations: []
+})
+export class SavingsModule { }
