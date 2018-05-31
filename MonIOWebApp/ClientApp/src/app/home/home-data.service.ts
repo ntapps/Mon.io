5855d00267415b256168d9862bef1dd910b4ff07
Garrett Young <garrett.young.dev@gmail.com>
2018-05-31 16:00:25 -0400

    HomeView component created

--- MonIOWebApp/ClientApp/src/app/home/home-data.service.ts
+++ MonIOWebApp/ClientApp/src/app/home/home-data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiDataService } from 'src/app/shared-data/api-data.service';
 import { HomeData } from 'src/app/models/HomeData';
-import { homedir } from 'os';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +15,18 @@ export class HomeDataService {
 
   GetAnyNumber(){
     return this.apiData.GetAnyNumber();
+  }
+
+  GetBudgets(){
+    return this.homeData.BudgetBalance;
+  }
+
+  GetExpenses(){
+    return this.homeData.ExpenseBalance;
+  }
+
+  GetSavings(){
+    return this.homeData.SavingsBalance;
   }
 
 
