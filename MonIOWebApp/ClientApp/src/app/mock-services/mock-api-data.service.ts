3cf2e7d02c61f78191decedf25c1bc66d418e9f2
Andrew Lehman <lehmanad1@live.com>
2018-06-19 21:53:55 -0400

    mock-api added, budget constructor

---
+++ MonIOWebApp/ClientApp/src/app/mock-services/mock-api-data.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HomeData } from '../models/HomeData';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Observable } from 'rxjs/internal/Observable';
+import { Budget } from 'src/app/models/Budget';
+//Service to fetch API data and disperse it to the relevant views
+@Injectable({
+  providedIn: 'root'
+})
+export class MockApiDataService {
+
+  constructor() {
+    this.dataStore = {
+      homeData: new HomeData(0,0,0),
+      budget: null
+    };
+    this._homeData = new BehaviorSubject<HomeData>(this.dataStore.homeData)
+
+    //call fetches on our dataStore objects
+    //(when we get an API)
+    //test purposes only. Remove when service is created.
+    setTimeout(() => {this.loadHomeData(100, 100, 100)}, 2000);
+  }
+
+  //instantiate our observables here
+  private _homeData: BehaviorSubject<HomeData>;
+
+  //store our model's data here to compare from api calls 
+  //and check for changes
+  private dataStore: {
+    homeData : HomeData;
+    budget: any;
+  }
+
+  loadHomeData(b:number, e:number, s:number){
+    this.dataStore.homeData = new HomeData(b,e,s);
+    this._homeData.next(Object.assign({}, this.dataStore).homeData);
+  }
+
+  get homeData() {
+    return Observable.of(new HomeData(10,10,10));
+  }
+
+  get budget(){
+    return Observable.of(new Budget(-1,1997,1,"testExpense", 10));
+  }
+}
--- MonIOWebApp/ClientApp/src/app/models/Budget.ts
+++ MonIOWebApp/ClientApp/src/app/models/Budget.ts
@@ -3,4 +3,14 @@ export class Budget {
   public id: Number;
   public year: Number;
   public month: Number;
+  public expenseName: string;
+  public expenseValue: number;
+
+  constructor(id:Number, year:Number, month:Number, expenseName:string, expenseValue:number){
+    this.id = id;
+    this.year = year;
+    this.month = month;
+    this.expenseName = expenseName;
+    this.expenseValue = expenseValue;
+  };
 }
